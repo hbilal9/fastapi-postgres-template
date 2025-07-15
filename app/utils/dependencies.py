98ce7395ee4c3a71f8f4f41437f7e3fed383afd5
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2025-07-15 10:54:41 +0500

    feat: refactor get_current_user_dependency to use AsyncSession for improved async handling and performance ⚡

--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, Request
 from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated, Union, Optional
 
 from app.utils.database import get_db
@@ -9,15 +9,15 @@ from app.features.auth.service import get_current_user, get_current_user_from_co
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token", auto_error=False)
 
-def get_current_user_dependency(
+async def get_current_user_dependency(
     request: Request,
     token: Optional[str] = Depends(oauth2_scheme),
-    db: Session = Depends(get_db)
+    db: AsyncSession = Depends(get_db)
 ) -> User:
     if token:
-        return get_current_user(token, db)
-    return get_current_user_from_cookie(request, db)
+        return await get_current_user(token, db)
+    return await get_current_user_from_cookie(request, db)
 
 # First Check on the Basis of Token and then on the Basis of Cookies
 CurrentUser = Annotated[User, Depends(get_current_user_dependency)]
-DbSession = Annotated[Session, Depends(get_db)]
+DbSession = Annotated[AsyncSession, Depends(get_db)]
