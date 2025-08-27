d0cd079367ae729328beca3bd8a9e37f54899574
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2025-08-27 15:49:39 +0500

    refactor: reorder parameters in get_current_user_dependency for clarity and consistency 🔄

--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -13,11 +13,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token", auto_error=Fals
 
 async def get_current_user_dependency(
     request: Request,
-    token: Optional[str] = Depends(oauth2_scheme),
-    db: Optional[AsyncSession] = None,
+    db: Annotated[AsyncSession, Depends(get_db)],
+    token: Annotated[Optional[str], Depends(oauth2_scheme)] = None,
 ) -> User:
-    if db is None:
-        db = await get_db()
     if token:
         return await get_current_user(token, db)
     return await get_current_user_from_cookie(request, db)
