3882b8d14000a89a1ad4fa729e37692c45b1a8ba
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2026-01-09 11:43:21 +0500

    refactor(constants): simplify string formatting and update 2FA to MFA

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -3,16 +3,12 @@ ACCESS_TOKEN_NAME = "access_token"  # nosec
 REFRESH_TOKEN_NAME = "refresh_token"  # nosec
 
 USER_NOT_FOUND_ERROR = "User not found"  # nosec
-EMAIL_ALREADY_EXISTS_ERROR = (
-    "If an account with this email already exists, "
-    "a password reset link has been sent."
-)  # nosec
+EMAIL_ALREADY_EXISTS_ERROR = "If an account with this email already exists, a password reset link has been sent."  # nosec
 INVALID_CREDENTIALS_ERROR = "Invalid credentials"  # nosec
 EMAIL_VERIFICATION_REQUIRED_ERROR = (
-    "Email verification required. "
-    "Please check your email inbox and verify your account."
-)  # nosec
-TWOFA_REQUIRED_ERROR = "2FA token required or invalid"  # nosec
+    "Email verification required. Please check your email inbox and verify your account."  # nosec
+)
+TWOFA_REQUIRED_ERROR = "MFA token required or invalid"  # nosec
 INVALID_VERIFICATION_TOKEN_ERROR = "Invalid verification token"  # nosec
 VERIFICATION_TOKEN_EXPIRED_ERROR = (
     "Verification token expired. "
